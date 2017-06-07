5881c19aaaaab79cd2a81f56527ff5fab6ff4bc1
Abdolmartin <ramtin.yazdanian@gmail.com>
2017-06-07 15:38:30 +0430

    new files added to git

---
+++ src/exceptions/InvalidArgumentException.java
@@ -0,0 +1,19 @@
+package exceptions;
+
+public class InvalidArgumentException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidArgumentException() {
+		super();
+	}
+
+	public InvalidArgumentException(String arg0) {
+		super(arg0);
+	}
+	
+	
+}
