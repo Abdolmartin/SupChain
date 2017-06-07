5b921f122d8bbf0c2fd8cf793e15bd55838789a6
Abdolmartin <ramtin.yazdanian@gmail.com>
2017-06-07 20:56:50 +0430

    test

--- src/ui/LoggedInMainPortal.java
+++ src/ui/LoggedInMainPortal.java
@@ -14,5 +14,6 @@ public abstract class LoggedInMainPortal extends JDialog {
 		JButton logOutButton = new JButton("\u062E\u0631\u0648\u062C \u0627\u0632 \u0633\u0627\u0645\u0627\u0646\u0647");
 		logOutButton.setBounds(340, 11, 123, 23);
 		getContentPane().add(logOutButton);
+		
 	}
 }
